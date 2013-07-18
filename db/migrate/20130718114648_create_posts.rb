69048bf2b1ca2722107f1f03ba688adc78f25110
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-18 12:01:08 -0700

    added posts to db

--- db/migrate/20130718114648_create_posts.rb
+++ db/migrate/20130718114648_create_posts.rb
@@ -1,10 +1,9 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      has_many :categories, through: :tags
-      t.string = :title
-      t.string = :content
-      t.string = :author
+      t.string :title
+      t.string :content
+      t.string :author
       t.timestamps
     end
   end
