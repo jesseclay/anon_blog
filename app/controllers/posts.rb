1c91b06ad7661255169069dfd45779471e4c4c2c
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-18 15:30:42 -0700

    added routing for viewing all posts, show posts for category, edit posts, and deleting posts

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -4,3 +4,20 @@ post '/create_post' do
               content: params[:content])
   erb :index
 end
+
+post '/find_individual' do
+
+end
+
+post '/display_all_by_category' do
+
+end
+
+post '/edit_existing' do
+
+end
+
+post '/delete' do
+
+end
+
