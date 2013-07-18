e0baceeef2fc3ae9ab5d3668c2fc8bc08b2f4da7
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-18 12:10:33 -0700

    filled in tags model and migration

---
+++ db/migrate/20130718120650_create_tags.rb
@@ -0,0 +1,10 @@
+class CreateTags < ActiveRecord::Migration
+  def change
+    create_table :tags do |t|
+      t.belongs_to :post
+      t.belongs_to :category
+      t.timestamps
+    end
+
+  end
+end
